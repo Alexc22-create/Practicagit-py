6d6c3238fbec1d8ba9ed2f8f3f23842febbd9879
Alexc22_create <20243rd035@utez.edu.mx>
2025-09-17 15:53:29 -0600

    subido

---
+++ gitscaner.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+import time
+import webbrowser
+import requests
+import winreg
+
+git_dir = '/home/alex/Documentos/Utez/4to/prog-red/practica'
+
+if not os.path.exists(git_dir):
+    os.makedirs(git_dir)
+    print(f"Directorio {git_dir} creado.")
+
+repos= [name for name in os.listdir('git_dir') if os.path.isdir(os.path.join(git_dir,name)) and os.path.exists(os.path.join(git_dir, name, '.git'))]
